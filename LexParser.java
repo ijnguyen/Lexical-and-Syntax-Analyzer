bba972d83fe1ba4933681c682c79e0e3b8f2468d
ijnguyen <94132772+ijnguyen@users.noreply.github.com>
2022-11-19 23:28:19 -0500

    Create LexParser.java

---
+++ LexParser.java
@@ -0,0 +1,24 @@
+
+public class LexParser {
+
+	// Operations
+	public static int ADD_OP = 1;
+	public static int SUB_OP = 2;
+	public static int MULT_OP = 3;
+	public static int DIV_OP = 4;
+	public static int MOD = 5;
+	public static int LESSTHAN = 6;
+	public static int GREATERTHAN = 7;
+	public static int LESSTHANOREQUAL = 8;
+	public static int GREATERTHANOREQUAL = 9;
+	public static int EQUALS_OP = 10;
+	public static int NOTEQUALS_OP = 11;
+	public static int ASSIGN = 12;
+	public static int LEFT_PAREN = 13;
+	public static int RIGHT_PAREN = 14;
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
